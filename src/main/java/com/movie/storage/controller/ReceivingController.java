3ff6e55a668d53dbd2bfb576f7cbe41317dcd00b
Oleksandr Omelchenko <alexmir_98@ukr.net>
2022-11-22 22:18:04 +0200

    implemented logging

--- src/main/java/com/movie/storage/controller/ReceivingController.java
+++ src/main/java/com/movie/storage/controller/ReceivingController.java
@@ -4,6 +4,7 @@ import com.movie.storage.entity.Type;
 import com.movie.storage.entity.Movie;
 import com.movie.storage.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
@@ -13,32 +14,37 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/movie-api/v1/getBy")
+@RequestMapping(value = "/movie-api/v1/movies")
 @RequiredArgsConstructor
+@Log4j2
 public class ReceivingController {
     private final MovieService movieService;
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping("/genre/{genre}")
     Slice<Movie> getMoviesByGenre(@PathVariable(value = "genre") String genre, Pageable pageable) {
+        log.info("Query for reading by genre with value: " + genre);
         return movieService.getMoviesByGenre(genre, pageable);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping("/title/{title}")
     Slice<Movie> getMoviesByTitle(@PathVariable(value = "title") String title, Pageable pageable) {
+        log.info("Query for reading by title with value: " + title);
         return movieService.getMoviesByTitle(title, pageable);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping("/type/{type}")
     Slice<Movie> getMoviesByType(@PathVariable(value = "type") Type type, Pageable pageable) {
+        log.info("Query for reading by type with value: " + type);
         return movieService.getMoviesByType(type, pageable);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping("/year/{year}")
     Slice<Movie> getMoviesByYear(@PathVariable(value = "year") int year, Pageable pageable) {
+        log.info("Query for reading by year with value: " + year);
         return movieService.getMoviesByYear(year, pageable);
     }
 }
