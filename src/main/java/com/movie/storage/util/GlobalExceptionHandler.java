23349fc15d0039b92f7633158a3fa5f94f01d099
Oleksandr Omelchenko <alexmir_98@ukr.net>
2022-11-22 22:18:14 +0200

    implemented logging

--- src/main/java/com/movie/storage/util/GlobalExceptionHandler.java
+++ src/main/java/com/movie/storage/util/GlobalExceptionHandler.java
@@ -1,34 +1,22 @@
 package com.movie.storage.util;
 
 import com.movie.storage.entity.AppError;
-import com.movie.storage.entity.Type;
-import com.movie.storage.exception.TypeParsingException;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 @RestControllerAdvice
 @Log4j2
+@Order(2)
 public class GlobalExceptionHandler {
-    @ExceptionHandler
-    public ResponseEntity<AppError> catchMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<AppError> catchException(Exception  e) {
         log.error(e.getMessage(), e);
-        return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(),
-                Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()),
-                HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<AppError> catchEnumTypeParsingException(TypeParsingException e) {
-        log.error(e.getMessage(), e);
-        return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(),
-                "Type movies must be one of: " +
-                        Arrays.toString(Type.values())), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new AppError(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Ooops, we are working on it!"),
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
---
+++ src/main/java/com/movie/storage/util/GlobalSpecificExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.movie.storage.util;
+
+import com.movie.storage.entity.AppError;
+import com.movie.storage.entity.Type;
+import com.movie.storage.exception.TypeParsingException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@RestControllerAdvice
+@Log4j2
+@Order(1)
+public class GlobalSpecificExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<AppError> catchMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.warn(e.getMessage(), e);
+        return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(),
+                Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({TypeParsingException.class})
+    public ResponseEntity<AppError> catchEnumTypeParsingException(TypeParsingException e) {
+        log.warn(e.getMessage(), e);
+        return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(),
+                "Type movies must be one of: " +
+                        Arrays.toString(Type.values())), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<AppError> catchNoHandlerFoundException(NoHandlerFoundException e) {
+        log.warn(e.getMessage(), e);
+        return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(),
+                "Page not found"),
+                HttpStatus.NOT_FOUND);
+    }
+}
